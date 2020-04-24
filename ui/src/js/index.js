e6992768a7b4773f0c648aaf7dc803557db55400
Méril Pilon <meril.pilon@gmail.com>
2020-04-24 11:27:53 +0100

    feat(telemetry): add worker to console

--- ui/src/js/index.js
+++ ui/src/js/index.js
@@ -1,1 +1,9 @@
 import "./console"
+import Worker from "./telemetry/worklet"
+
+const worker = new Worker()
+
+worker.onmessage = (event) => {
+  console.log("received: " + event.data)
+}
+worker.postMessage(42)
